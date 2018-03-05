f66525964ef87f91b78ef14a56aadda33a882fcf
40180378 <fmcnamee04@qub.ac.uk>
2018-03-05 02:07:30 +0000

    Added source code
    Added source code

---
+++ README.md
@@ -0,0 +1,6 @@
+Simple Python Web Server
+========================
+
+This is the source code for howCode's simple Python Web Server.
+
+You can watch the video that accompanies this source code here: https://youtu.be/hFNZ6kdBgO0
---
+++ serv.py
@@ -0,0 +1,20 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class Serv(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+
+httpd = HTTPServer(('localhost', 8080), Serv)
+httpd.serve_forever()
